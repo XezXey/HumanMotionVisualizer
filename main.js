e36897be50179161201b35f6e6182e7e7709f59c
XezXey <puntawat-1@hotmail.com>
2025-07-17 16:42:59 +0700

    add

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-// import * as THREE from "./node_modules/three";
-// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-// import { Reflector } from "three/examples/jsm/objects/Reflector.js";
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-// import GUI from "./node_modules/lil-gui";
-// import { all, color, split } from "./node_modules/three/src/nodes/TSL.js";
+import * as THREE from "./node_modules/three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { Reflector } from "three/examples/jsm/objects/Reflector.js";
+import GUI from "./node_modules/lil-gui";
 
 // import * as THREE from "https://cdn.skypack.dev/three";
 // import { OrbitControls } from "https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js";
@@ -13,15 +11,15 @@
 // import { all, color, split } from "https://cdn.skypack.dev/three/src/nodes/TSL.js";
 
 // Three.js core
-import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.js";
-// Controls, Loaders, Reflector from examples
-import { OrbitControls } from "https://unpkg.com/three@0.158.0/examples/jsm/controls/OrbitControls.js";
-import { Reflector } from "https://unpkg.com/three@0.158.0/examples/jsm/objects/Reflector.js";
-import { GLTFLoader } from "https://unpkg.com/three@0.158.0/examples/jsm/loaders/GLTFLoader.js";
-// lil-gui (modern GUI for dat.GUI)
-import GUI from "https://cdn.jsdelivr.net/npm/lil-gui@0.18/+esm";
-// TSL nodes from Three.js (advanced shader nodes system)
-import { all, color, split } from "https://unpkg.com/three@0.158.0/examples/jsm/nodes/Nodes.js";
+// import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.js";
+// // Controls, Loaders, Reflector from examples
+// import { OrbitControls } from "https://unpkg.com/three@0.158.0/examples/jsm/controls/OrbitControls.js";
+// import { Reflector } from "https://unpkg.com/three@0.158.0/examples/jsm/objects/Reflector.js";
+// import { GLTFLoader } from "https://unpkg.com/three@0.158.0/examples/jsm/loaders/GLTFLoader.js";
+// // lil-gui (modern GUI for dat.GUI)
+// import GUI from "https://cdn.jsdelivr.net/npm/lil-gui@0.18/+esm";
+// // TSL nodes from Three.js (advanced shader nodes system)
+// import { all, color, split } from "https://unpkg.com/three@0.158.0/examples/jsm/nodes/Nodes.js";
 
 // import * as THREE from "three";
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
